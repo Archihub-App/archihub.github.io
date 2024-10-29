5cd23639742f1e3d6bf04fafbcfeb661b0bfdd82
Nestor Pena <nestor2509@gmail.com>
2024-10-28 19:57:41 -0500

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 			logo: {
 				src: './src/assets/logo_docu_archihub.png',
 			},
-			favicon: './src/assets/favicon.ico',
+			favicon: '/archihub.github.io/favicon.ico',
 			social: {
 				github: 'https://github.com/Archihub-App/archihub.github.io',
 				instagram: 'https://www.instagram.com/archihub_app/',
